68e24c1793c5f27dffc1415bcaabe28162e4910c
Antoine Augusti <antoine.augusti@gmail.com>
2019-01-08 09:38:31 +0100

    Path to data file

--- analyses/regulation.py
+++ analyses/regulation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('tweets.csv', parse_dates=['created_at']).set_index('id', drop=False)
+df = pd.read_csv('../tweets.csv', parse_dates=['created_at']).set_index('id', drop=False)
 df['numero_semaine'] = df.apply(lambda row: row['created_at'].strftime('%G-%V'), axis=1)
 df['est_en_reponse'] = df.apply(lambda row: row['text'].startswith('@'), axis=1)
 df[['numero_semaine', 'est_en_reponse', 'tweet_mentionne_regulation']].to_csv('/tmp/data.csv', index=False)
