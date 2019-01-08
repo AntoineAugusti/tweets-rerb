7e1fd57683030770d5a2ab2bf1a7ab22e952b987
Antoine Augusti <antoine.augusti@gmail.com>
2019-01-08 09:34:23 +0100

    Add file for regulation

--- analyses/chart.py
+++ analyses/chart.py
@@ -4,7 +4,7 @@ import pandas as pd
 cols = ['tweet_mentionne_excuse', 'tweet_mentionne_bon_courage']
 
 df = pd.read_csv('tweets.csv', parse_dates=['created_at']).set_index('id', drop=False)
-data = pd.DataFrame(df.groupby(df.created_at.dt.strftime('%Y-%U'))[cols].sum().stack().rename('value'))
+data = pd.DataFrame(df.groupby(df.created_at.dt.strftime('%G-%V'))[cols].sum().stack().rename('value'))
 data.index.names = ['date', 'type']
 data.reset_index(inplace=True)
 
